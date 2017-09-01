eb44b627db61e448f2ae36fb4b585c144a2385db
jagaa <jagaa1103@gmail.com>
2017-09-01 15:59:50 +0900

    project init and added recordBtn and RecordongService

---
+++ AudioRecorder/AudioRecorder/RecordingService.swift
@@ -0,0 +1,14 @@
+//
+//  RecordingService.swift
+//  AudioRecorder
+//
+//  Created by Enkhjargal Gansukh on 01/09/2017.
+//  Copyright © 2017 Enkhjargal Gansukh. All rights reserved.
+//
+
+import Foundation
+
+
+class RecordingService {
+    
+}
---
+++ AudioRecorder/AudioRecorder/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AudioRecorder
+//
+//  Created by Enkhjargal Gansukh on 01/09/2017.
+//  Copyright © 2017 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func recordBtnClicked(_ sender: Any) {
+        
+    }
+    
+}
+
